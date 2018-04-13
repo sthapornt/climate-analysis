532792191e9c972dcd61d03a45b4399b781798e7
sthapornt <sthapornt@yahoo.com>
2018-04-13 14:12:24 +0100

    Add Docstring

--- climate_analysis.py
+++ climate_analysis.py
@@ -1,3 +1,5 @@
+""Initial commit of climate analysis code""
+
 import sys
 import temp_conversion
 import signal
@@ -25,3 +27,4 @@ for line in climate_data:
             kelvin = temp_conversion.fahr_to_kelvin(fahr)
 
             print(str(celsius)+", "+str(kelvin))
+# TODO[js-fdsa]
